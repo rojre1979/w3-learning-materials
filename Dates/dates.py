8c51dc63cb06bcf92cb8dd3eeae779867c184f69
Robert Regalado <rjregalado1979@gmail.com>
2023-01-25 19:42:54 +0800

    added strftime method in line 22

--- Dates/dates.py
+++ Dates/dates.py
@@ -2,3 +2,21 @@ import datetime
 
 x = datetime.datetime.now()
 print(x)
+
+'''
+Return the year and name of weekday:
+'''
+print(x.year)
+print(x.strftime("%A"))
+
+# Creating Date Objects
+
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+# The strftime() Method
+
+# Display the name of the month
+
+x = datetime.datetime(2018, 6, 1)
+print(x.strftime("%B"))
